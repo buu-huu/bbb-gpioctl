738d322131bda89cb25f9cf363db9a24b8aa2d80
buu-huu <luca-schwarz@outlook.de>
2021-04-20 21:34:27 +0200

    Added test GPIO file and function for reading file to a string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let inhalt = read_file("gpio02");
+    println!("{}", inhalt);
+}
+
+fn read_file(path: &str) -> String {
+    let path = Path::new(path);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    file.read_to_string(&mut s).expect("Error reading file!");
+    return s;
 }
