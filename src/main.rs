3b5077854bcb4254b7b3ac4b7e306537af3c597d
buu-huu <luca-schwarz@outlook.de>
2021-04-21 23:35:08 +0200

    Argument parsing base and new function for reading gpio file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::env;
+
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
+use std::io::Read;
 use std::path::Path;
 
 fn main() {
-    let inhalt = read_file("gpio02");
-    println!("{}", inhalt);
-}
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        println!("Specify at least 3 Arguments");
+        return;
+    }
+    
 
+}
+/*
 fn read_file(path: &str) -> String {
     let path = Path::new(path);
     let display = path.display();
@@ -19,4 +28,25 @@ fn read_file(path: &str) -> String {
     let mut s = String::new();
     file.read_to_string(&mut s).expect("Error reading file!");
     return s;
+}
+*/
+
+fn read_gpio(gpio: &str) -> Result<String, io::Error> {
+    let f = File::open(gpio);
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e)   => return Err(e)
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_)  => Ok(s),
+        Err(e) => Err(e)
+    }
+}
+
+
+enum AvailableGpios {
+    gpio01,
+    gpio02
 }
